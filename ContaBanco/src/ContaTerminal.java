9bf4740333f1895db5161e3912b8df5bd53f7e74
TayEsp <tayna.respinosa@gmail.com>
2025-08-15 17:04:26 -0300

    Criando uma interface para banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        var scanner = new Scanner(System.in);
+        System.out.print("Por favor, Insira o número da conta: ");
+        var numConta = scanner.nextInt();
+        System.out.print("Por favor, Insira o número da agência: ");
+        var numAgencia = scanner.next();
+        System.out.print("Por favor, Insira seu nome: ");
+        var nome = scanner.next();
+        System.out.print("Por favor, Insira o saldo que deseja sacar: ");
+        var saldo = scanner.nextFloat();
+
+        System.out.println("\nOlá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + numAgencia +
+        ", conta " + numConta + " e seu saldo R$" + saldo + " já esta disponível para saque.");
+
+        scanner.close();
     }
 }
